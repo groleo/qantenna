06e96ac0c4ff0b923974ed0271b84c9a246bc34d
Lisandro Damián Nicanor Pérez Meyer <perezmeyer@gmail.com>
2010-12-19 18:35:56 -0300

    Fix NENH cards indentation.

--- src/cards/nenhcards.h
+++ src/cards/nenhcards.h
@@ -25,73 +25,73 @@
 #include <QString>
 
 /**
-	NE and NH cards: Near Fields
-	Request calculation of near electric fields in the vicinity of the antenna
-	(NE) and to request near magnetic fields (NH).
+  NE and NH cards: Near Fields
+  Request calculation of near electric fields in the vicinity of the antenna
+  (NE) and to request near magnetic fields (NH).
 */
 
 class NENHCards : public GenericCard
 {
 public:
-	NENHCards(bool isItNECard, int theCoordinateSystemType,
-	          int theNumberOfPointsXR, int theNumberOfPointsYPhi,
-	          int theNumberOfPointsZTheta, double theCoordinateXR,
-	          double theCoordinateYPhi, double theCoordinateZTheta,
-	          double theIncrementXR, double theIncrementYPhi,
-	          double theIncrementZTheta);
+  NENHCards(bool isItNECard, int theCoordinateSystemType,
+            int theNumberOfPointsXR, int theNumberOfPointsYPhi,
+            int theNumberOfPointsZTheta, double theCoordinateXR,
+            double theCoordinateYPhi, double theCoordinateZTheta,
+            double theIncrementXR, double theIncrementYPhi,
+            double theIncrementZTheta);
 
-	bool getIsNECard() const;
-	int getCoordinateSystemType() const;
-	int getNumberOfPointsXR() const;
-	int getNumberOfPointsYPhi() const;
-	int getNumberOfPointsZTheta() const;
-	double getCoordinateXR() const;
-	double getCoordinateYPhi() const;
-	double getCoordinateZTheta() const;
-	double getIncrementXR() const;
-	double getIncrementYPhi() const;
-	double getIncrementZTheta() const;
+  bool getIsNECard() const;
+  int getCoordinateSystemType() const;
+  int getNumberOfPointsXR() const;
+  int getNumberOfPointsYPhi() const;
+  int getNumberOfPointsZTheta() const;
+  double getCoordinateXR() const;
+  double getCoordinateYPhi() const;
+  double getCoordinateZTheta() const;
+  double getIncrementXR() const;
+  double getIncrementYPhi() const;
+  double getIncrementZTheta() const;
 
 QString getCard();
 
 private:
-	/**
-		true = NE card
-		false = NH card
-	*/
-	bool isNECard;
-	/**
-		0 - rectangular coordinates will be used.
-		1 - spherical coordinates will be used.
-	*/
-	int coordinateSystemType;
-	/**
-		coordinateSystemType ==
-			0: Number of points desired in the X
-			1: Number of points desired in the r
-		The value 1 is assumed for any field left blank.
-	*/
-	int numberOfPointsXR;
-	/**
-		coordinateSystemType ==
-			0: Number of points desired in the Y
-			1: Number of points desired in the Phi
-		The value 1 is assumed for any field left blank.
-	*/
-	int numberOfPointsYPhi;
-	/**
-		coordinateSystemType ==
-			0: Number of points desired in the Z
-			1: Number of points desired in the Theta
-		The value 1 is assumed for any field left blank.
-	*/
-	int numberOfPointsZTheta;
-	double coordinateXR;
-	double coordinateYPhi;
-	double coordinateZTheta;
-	double incrementXR;
-	double incrementYPhi;
-	double incrementZTheta;
+  /**
+    true = NE card
+    false = NH card
+  */
+  bool isNECard;
+  /**
+    0 - rectangular coordinates will be used.
+    1 - spherical coordinates will be used.
+  */
+  int coordinateSystemType;
+  /**
+    coordinateSystemType ==
+      0: Number of points desired in the X
+      1: Number of points desired in the r
+    The value 1 is assumed for any field left blank.
+  */
+  int numberOfPointsXR;
+  /**
+    coordinateSystemType ==
+      0: Number of points desired in the Y
+      1: Number of points desired in the Phi
+    The value 1 is assumed for any field left blank.
+  */
+  int numberOfPointsYPhi;
+  /**
+    coordinateSystemType ==
+      0: Number of points desired in the Z
+      1: Number of points desired in the Theta
+    The value 1 is assumed for any field left blank.
+  */
+  int numberOfPointsZTheta;
+  double coordinateXR;
+  double coordinateYPhi;
+  double coordinateZTheta;
+  double incrementXR;
+  double incrementYPhi;
+  double incrementZTheta;
 };
 
 #endif // NENHCARDS_H
