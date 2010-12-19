47e5a3186f82c3747dd350738b03e0f3afd8b15b
Lisandro Damián Nicanor Pérez Meyer <perezmeyer@gmail.com>
2010-12-19 17:45:43 -0300

    Fix indentation in SC card.

--- src/cards/sccard.h
+++ src/cards/sccard.h
@@ -29,40 +29,40 @@
 class SCCard : public GenericCard
 {
 public:
-	/// Following a SP card with just one more coordinate needed
-	SCCard(int thePatchShape, double theXCoordinateCorner3,
-	       double theYCoordinateCorner3, double theZCoordinateCorner3);
-	/// Following a SM card (no patchShape)
-	SCCard(double theXCoordinateCorner3, double theYCoordinateCorner3,
-	       double theZCoordinateCorner3);
-	/// Following a SP card with two more coordinate needed
-	SCCard(int thePatchShape, double theXCoordinateCorner3,
-	       double theYCoordinateCorner3, double theZCoordinateCorner3,
-	       double theXCoordinateCorner4, double theYCoordinateCorner4,
-	       double theZCoordinateCorner4);
+  /// Following a SP card with just one more coordinate needed
+  SCCard(int thePatchShape, double theXCoordinateCorner3,
+         double theYCoordinateCorner3, double theZCoordinateCorner3);
+  /// Following a SM card (no patchShape)
+  SCCard(double theXCoordinateCorner3, double theYCoordinateCorner3,
+         double theZCoordinateCorner3);
+  /// Following a SP card with two more coordinate needed
+  SCCard(int thePatchShape, double theXCoordinateCorner3,
+         double theYCoordinateCorner3, double theZCoordinateCorner3,
+         double theXCoordinateCorner4, double theYCoordinateCorner4,
+         double theZCoordinateCorner4);
 
-	bool getPatchShapeExists() const;
-	bool getFourCorners() const;
-	int getPatchShape() const;
-	double getXCoordinateCorner3() const;
-	double getYCoordinateCorner3() const;
-	double getZCoordinateCorner3() const;
-	double getXCoordinateCorner4() const;
-	double getYCoordinateCorner4() const;
-	double getZCoordinateCorner4() const;
+  bool getPatchShapeExists() const;
+  bool getFourCorners() const;
+  int getPatchShape() const;
+  double getXCoordinateCorner3() const;
+  double getYCoordinateCorner3() const;
+  double getZCoordinateCorner3() const;
+  double getXCoordinateCorner4() const;
+  double getYCoordinateCorner4() const;
+  double getZCoordinateCorner4() const;
 
-	QString getCard();
+  QString getCard();
 
 private:
-	bool patchShapeExists;
-	bool fourCorners;
-	int patchShape;
-	double xCoordinateCorner3;
-	double yCoordinateCorner3;
-	double zCoordinateCorner3;
-	double xCoordinateCorner4;
-	double yCoordinateCorner4;
-	double zCoordinateCorner4;
+  bool patchShapeExists;
+  bool fourCorners;
+  int patchShape;
+  double xCoordinateCorner3;
+  double yCoordinateCorner3;
+  double zCoordinateCorner3;
+  double xCoordinateCorner4;
+  double yCoordinateCorner4;
+  double zCoordinateCorner4;
 };
 
 #endif // SCCARD_H
