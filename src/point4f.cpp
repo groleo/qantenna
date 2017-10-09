284d477ec3ae76f9638af3d284b38565f587e4ba
Adrian Negreanu <adrian.negreanu@nxp.com>
2017-10-09 10:39:32 +0300

    qt5: fix ambiguous overload errors
    point4f.cpp:49:7: error: ambiguous overload for ‘operator<<’ (operand
    types are ‘QDataStream’ and ‘qreal {aka double}’)
         s << p.x() << p.y() << p.z() << p.w();

--- src/point4f.cpp
+++ src/point4f.cpp
@@ -22,6 +22,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 #include <QDebug>
+#include <QDataStream>
 
 #include "point4f.h"
 
